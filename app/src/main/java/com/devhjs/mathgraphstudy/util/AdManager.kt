fd20b2d7f9124998d95b8b680b56aebaa20f30ee
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-01-10 16:17:47 +0900

    feat: 빌드 분기를 통한 AD TEST ID 처리

--- app/src/main/java/com/devhjs/mathgraphstudy/util/AdManager.kt
+++ app/src/main/java/com/devhjs/mathgraphstudy/util/AdManager.kt
@@ -1,16 +1,26 @@
 package com.devhjs.mathgraphstudy.util
  
  import android.app.Activity
- import android.content.Context
- import android.util.Log
+import android.content.Context
+import android.util.Log
+ import com.devhjs.mathgraphstudy.BuildConfig
  import com.google.android.gms.ads.AdRequest
- import com.google.android.gms.ads.LoadAdError
- import com.google.android.gms.ads.MobileAds
- import com.google.android.gms.ads.interstitial.InterstitialAd
- import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
- 
- object AdManager {
-     private const val AD_UNIT_ID = "ca-app-pub-3216980827282944/1022276279"
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+object AdManager {
+    // 빌드 분기를 통한 Test ID 처리
+    private val AD_UNIT_ID = if (BuildConfig.DEBUG) {
+        "ca-app-pub-3940256099942544/1033173712" // Test ID
+    } else {
+        "ca-app-pub-3216980827282944/1022276279" // Real ID
+    }
+    // 비공개 테스트용 광고 ID 처리
+//     private val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+
+
      private var interstitialAd: InterstitialAd? = null
      private var isAdLoading = false
  
@@ -47,8 +57,8 @@ package com.devhjs.mathgraphstudy.util
      fun showInterstitial(activity: Activity) {
          if (interstitialAd != null) {
              interstitialAd?.show(activity)
-             interstitialAd = null // Ad shown, reset
-             loadInterstitial(activity) // Preload next ad
+             interstitialAd = null
+             loadInterstitial(activity)
          } else {
              Log.d("AdManager", "The interstitial ad wasn't ready yet.")
              loadInterstitial(activity)
